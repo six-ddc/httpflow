d530b0728db6852da5b61c0bb420309c96064dda
Forest Johnson <fjohnson@peoplenetonline.com>
2018-12-13 12:11:19 -0600

    trying to add ability to display binary body in base64

--- util.cpp
+++ util.cpp
@@ -2,6 +2,46 @@
 
 bool is_atty = true;
 
+// from https://gist.github.com/tomykaira/f0fd86b6c73063283afe550bc5d77594
+std::string encode_base64(const std::string data) {
+  constexpr char sEncodingTable[] = {
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+      'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+      'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+      'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+      'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+      'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+      'w', 'x', 'y', 'z', '0', '1', '2', '3',
+      '4', '5', '6', '7', '8', '9', '+', '/'
+  };
+  size_t in_len = data.size();
+  size_t out_len = 4 * ((in_len + 2) / 3);
+  std::string ret(out_len, '\0');
+  size_t i;
+  char *p = const_cast<char*>(ret.c_str());
+
+  for (i = 0; i < in_len - 2; i += 3) {
+    *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
+    *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int) (data[i + 1] & 0xF0) >> 4)];
+    *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2) | ((int) (data[i + 2] & 0xC0) >> 6)];
+    *p++ = sEncodingTable[data[i + 2] & 0x3F];
+  }
+  if (i < in_len) {
+    *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
+    if (i == (in_len - 1)) {
+      *p++ = sEncodingTable[((data[i] & 0x3) << 4)];
+      *p++ = '=';
+    }
+    else {
+      *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int) (data[i + 1] & 0xF0) >> 4)];
+      *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2)];
+    }
+    *p++ = '=';
+  }
+
+  return ret;
+}
+
 bool is_plain_text(const std::string &s) {
 	// The algorithm works by dividing the set of bytecodes [0..255] into three
 	// categories:
